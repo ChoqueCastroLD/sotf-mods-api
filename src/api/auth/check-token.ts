fc97139e2031ca18c301c1b7a5871790c46b5aff
Luis Choque <luis.choque.castro@outlook.com>
2023-09-21 01:49:52 -0500

    Added check token

---
+++ src/api/auth/check-token.ts
@@ -0,0 +1,30 @@
+import { Elysia, t } from 'elysia'
+
+import { prisma } from "../../services/prisma";
+import { authMiddleware } from '../../middlewares/auth.middleware'
+
+
+export const router = new Elysia()
+    .use(authMiddleware({ loggedOnly: true }))
+    .get(
+        '/api/auth/check',
+        async ({ token }) => {
+            const user = await prisma.user.findFirst({
+                where: {
+                    tokens: {
+                        some: {
+                            token
+                        }
+                    }
+                },
+                select: {
+                    name: true,
+                    slug: true,
+                    email: true,
+                    image_url: true,
+                }
+            });
+
+            return user;
+        }
+    )
