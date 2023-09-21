f3a5e19541b718b55fe28fb1d39f0796cbae41ec
Luis Choque <luis.choque.castro@outlook.com>
2023-09-21 04:54:14 -0500

    Added get user by slug endpoint

---
+++ src/api/users/get.ts
@@ -0,0 +1,28 @@
+import { Elysia, NotFoundError, t } from 'elysia'
+
+import { prisma } from '../../services/prisma'
+
+
+export const router = new Elysia()
+    .get(
+        '/api/users/:user_slug',
+        async ({ params: { user_slug } }) => {
+            const user = await prisma.user.findFirst({
+                where: {
+                    slug: user_slug,
+                },
+                select: {
+                    name: true,
+                    slug: true,
+                    email: true,
+                    image_url: true,
+                }
+            })
+
+            if (!user) {
+                throw new NotFoundError()
+            }
+
+            return user
+        }
+    )
