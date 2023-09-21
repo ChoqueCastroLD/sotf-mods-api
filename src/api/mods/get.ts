6fd490f69ad148674b1edfaee05e0c0a753b3fa8
Luis Choque <luis.choque.castro@outlook.com>
2023-09-21 16:54:02 -0500

    Order mod versions

--- src/api/mods/get.ts
+++ src/api/mods/get.ts
@@ -38,7 +38,7 @@ export const router = new Elysia()
                     },
                     versions: {
                         orderBy: {
-                            version: "asc",
+                            version: "desc",
                         },
                         include: {
                             downloads: {
