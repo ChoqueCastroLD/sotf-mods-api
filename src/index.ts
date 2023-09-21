797a3c0652bc26fc88dabe586bee4341e6915f6a
Luis Choque <luis.choque.castro@outlook.com>
2023-09-21 00:29:05 -0500

    Added logger

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { Elysia } from 'elysia'
 
 import { router } from './router'
 import { errorHandler } from './handlers/error.handler'
+import { loggerPlugin } from "./plugins/logger.plugin"
 
 
 const app = new Elysia()
+    .use(loggerPlugin)
     .use(errorHandler)
     .use(router)
     .listen(Bun.env.PORT ?? 3000)
