f5919a55ba7528f3aae71ae5263197d0c5d05af8
Luis Choque <luis.choque.castro@outlook.com>
2023-09-26 02:27:24 -0500

    Added approve endpoint

---
+++ src/api/mods/approve.ts
@@ -0,0 +1,42 @@
+import { Elysia, NotFoundError, t } from 'elysia'
+import semver from 'semver';
+
+import { prisma } from '../../services/prisma';
+import { authMiddleware } from '../../middlewares/auth.middleware'
+
+
+export const router = new Elysia()
+    .use(authMiddleware({ loggedOnly: true }))
+    .get(
+        '/api/mods/:user_slug/:mod_slug/approve',
+        async ({ params: { user_slug, mod_slug }, user }) => {
+            if (user?.canApprove !== true) {
+              throw new NotFoundError();
+            }
+            const mod = await prisma.mod.findFirst({
+              where: {
+                slug: mod_slug,
+                user: {
+                  slug: user_slug,
+                }
+              }
+            });
+            if (!mod) {
+              throw new NotFoundError();
+            }
+            await prisma.mod.update({
+              where: {
+                id: mod.id,
+              },
+              data: {
+                isApproved: true,
+              },
+            });
+            return { approved: true, message: 'Mod approved.' };
+        }, {
+            response: t.Object({
+              approved: t.Boolean(),
+              message: t.String(),
+            }),
+        }
+    )
