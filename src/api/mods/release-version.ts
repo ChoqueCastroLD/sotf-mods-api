eb40f1bfb7efda0067cc32b53c60229d63ac78d9
Luis Choque <luis.choque.castro@outlook.com>
2023-10-01 01:42:00 -0500

    new mod upload flow

--- src/api/mods/release-version.ts
+++ src/api/mods/release-version.ts
@@ -5,6 +5,7 @@ import { prisma } from '../../services/prisma';
 import { authMiddleware } from '../../middlewares/auth.middleware'
 import { ValidationError } from '../../errors/validation';
 import { uploadFile } from '../../services/files';
+import { readManifest } from '../../shared/read-manifest';
 
 
 const MOD_FILE_SIZE_LIMIT = 80 * 1024 * 1024; // 80MB
@@ -13,7 +14,7 @@ export const router = new Elysia()
     .use(authMiddleware({ loggedOnly: true }))
     .post(
         '/api/mods/:user_slug/:mod_slug/release',
-        async ({ params: { user_slug, mod_slug }, body: { version, changelog, modFile }, user }) => {
+        async ({ params: { user_slug, mod_slug }, body: { changelog, modFile }, user }) => {
           const mod = await prisma.mod.findFirst({
             where: {
               slug: mod_slug,
@@ -28,8 +29,13 @@ export const router = new Elysia()
           if (mod.userId !== user?.id) {
             throw new ValidationError([{ field: 'user', message: "You are not the owner of this mod." }])
           }
+          const file = await modFile.arrayBuffer()
+
+          const manifest = readManifest(file)
+          const version = manifest.version
+
           if (!semver.valid(version)) {
-            throw new ValidationError([{ field: 'version', message: "Invalid mod version provided." }])
+            throw new ValidationError([{ field: 'version', message: "Invalid mod version provided in manifest.json." }])
           }
 
           const existingVersion = await prisma.modVersion.findFirst({
@@ -48,62 +54,61 @@ export const router = new Elysia()
               isLatest: true,
             }
           });
-            if (latestVersion && !semver.gt(version, latestVersion.version)) {
-              throw new ValidationError([{ field: 'version', message: "Version must be greater than latest version." }])
-            }
+          if (latestVersion && !semver.gt(version, latestVersion.version)) {
+            throw new ValidationError([{ field: 'version', message: "Version must be greater than latest version." }])
+          }
 
-            const ext = modFile.name.split('.').pop();
-            if (ext !== 'zip' && ext !== 'dll') {
-              throw new ValidationError([{ field: 'modFile', message: "Mod file must be a zip or dll file." }])
-            }
+          const ext = modFile.name.split('.').pop();
+          if (ext !== 'zip') {
+            throw new ValidationError([{ field: 'modFile', message: "Mod file must be a zip file." }])
+          }
 
-            let filename = "";
-            try {
-              filename = await uploadFile(await modFile.arrayBuffer(), `${mod.slug}_${version}.${ext}`);
-              if (!filename) throw filename;
-            } catch (error) {
-              console.error(error);
-              throw new Error("An error occurred during file upload.");
-            }
+          let filename = "";
+          try {
+            filename = await uploadFile(file, `${mod.slug}_${version}.${ext}`);
+            if (!filename) throw filename;
+          } catch (error) {
+            console.error(error);
+            throw new Error("An error occurred during file upload.");
+          }
 
-            await prisma.modVersion.updateMany({
-              where: {
-                modId: mod.id,
-              },
-              data: {
-                isLatest: false,
-              }
-            })
-        
-            await prisma.modVersion.create({
-              data: {
-                version,
-                changelog,
-                downloadUrl: `${Bun.env.FILE_DOWNLOAD_ENDPOINT}/${filename}`,
-                isLatest: true,
-                filename,
-                extension: ext,
-                mod: {
-                  connect: {
-                    id: mod.id,
-                  }
+          await prisma.modVersion.updateMany({
+            where: {
+              modId: mod.id,
+            },
+            data: {
+              isLatest: false,
+            }
+          })
+      
+          await prisma.modVersion.create({
+            data: {
+              version,
+              changelog,
+              downloadUrl: `${Bun.env.FILE_DOWNLOAD_ENDPOINT}/${filename}`,
+              isLatest: true,
+              filename,
+              extension: ext,
+              mod: {
+                connect: {
+                  id: mod.id,
                 }
               }
-            })
-        
-            await prisma.mod.update({
-              where: {
-                id: mod.id,
-              },
-              data: {
-                updatedAt: new Date(),
-              }
-            })
+            }
+          })
+      
+          await prisma.mod.update({
+            where: {
+              id: mod.id,
+            },
+            data: {
+              updatedAt: new Date(),
+            }
+          })
 
-            return { released : true }
+          return { released : true }
         }, {
             body: t.Object({
-              version: t.String(),
               changelog: t.String({ minLength: 1, maxLength: 200 }),
               modFile: t.File({ minSize: 1, maxSize: MOD_FILE_SIZE_LIMIT }),
             }),
--- src/api/mods/upload.ts
+++ src/api/mods/upload.ts
@@ -1,142 +1,43 @@
-import { Elysia, NotFoundError, t } from 'elysia'
-import slugify from "slugify";
-import semver from 'semver';
-import sharp from "sharp";
+import { Elysia, t } from 'elysia'
+import semver from 'semver'
 
-import { prisma } from '../../services/prisma';
 import { authMiddleware } from '../../middlewares/auth.middleware'
-import { validateModDescription, validateModName, validateModShortDescription } from '../../shared/validation';
-import { ValidationError } from '../../errors/validation';
-import { uploadFile } from '../../services/files';
+import { ValidationError } from '../../errors/validation'
+import { readManifest } from '../../shared/read-manifest';
 
 
 const MOD_FILE_SIZE_LIMIT = 80 * 1024 * 1024; // 80MB
-const ALLOWED_RESOLUTIONS = [
-  { width: 2560, height: 1440 },
-  { width: 1080, height: 608 }
-];
 
 export const router = new Elysia()
     .use(authMiddleware({ loggedOnly: true }))
     .post(
         '/api/mods/upload',
-        async ({ body: { name, shortDescription, description, version, isNSFW, category_id, modFile, modThumbnail }, user }) => {
-          const errors = []
-
-          errors.push(...validateModName(name))
-          errors.push(...validateModDescription(description))
-          errors.push(...validateModShortDescription(shortDescription))
-          if (!semver.valid(version)) {
-            errors.push({ field: 'version', message: "Invalid mod version provided." })
-          }
-
-          if (errors.length > 0) {
-            throw new ValidationError(errors)
-          }
-
-          const image = await sharp(await modThumbnail.arrayBuffer()).metadata()
-
-          if (!ALLOWED_RESOLUTIONS.find((res) => res.width === image.width && res.height === image.height)) {
-            throw new ValidationError([{ field: 'modThumbnail', message: "Invalid thumbnail resolution" }])
-          }
-          
+        async ({ body: { modFile } }) => {
           const file = await modFile.arrayBuffer()
 
           if ((file.byteLength / 1024) > MOD_FILE_SIZE_LIMIT) {
             throw new ValidationError([{ field: 'modFile', message: "Mod file size exceeds the limit of 10MB." }])
           }
 
-          const slug = slugify(name, { lower: true });
-          const existingMod = await prisma.mod.findFirst({
-            where: {
-              OR: [
-                { name },
-                { slug },
-              ]
-            }
-          });
-          if (existingMod) {
-            throw new ValidationError([{ field: 'name', message: "Mod already exists." }])
-          }
-
-          const mod = await prisma.mod.create({
-            data: {
-              name,
-              slug,
-              shortDescription,
-              description,
-              isNSFW: isNSFW === "true",
-              isApproved: false,
-              isFeatured: false,
-              categoryId: parseInt(category_id),
-              userId: user?.id,
-            }
-          })
-
-          let thumbnailFilename;
-          try {
-            const ext = modThumbnail.name.split('.').pop()
-            thumbnailFilename = await uploadFile(await modThumbnail.arrayBuffer(), `${slug}_thumbnail.${ext}`)
-            if (!thumbnailFilename) throw thumbnailFilename;
-          } catch (error) {
-            console.error("Error uploading thumbnail:", error)
-            throw new ValidationError([{ field: 'modThumbnail', message: "An error occurred during thumbnail upload." }])
-          }
-
-          await prisma.modImage.create({
-            data: {
-              url: `${Bun.env.FILE_DOWNLOAD_ENDPOINT}/${thumbnailFilename}`,
-              isPrimary: false,
-              isThumbnail: true,
-              mod: {
-                connect: {
-                  id: mod.id,
-                }
-              }
-            }
-          });
-
           const ext = modFile.name.split('.').pop()
-          if (ext !== 'zip' && ext !== 'dll') {
-            throw new ValidationError([{ field: 'modFile', message: "Mod file must be a zip or dll file." }])
+          if (ext !== 'zip') {
+            throw new ValidationError([{ field: 'modFile', message: "Mod file must be a zip file." }])
           }
 
-          let filename;
-          try {
-            filename = await uploadFile(await modFile.arrayBuffer(), `${slug}_${version}.${ext}`)
-            if (!filename) throw filename;
-          } catch (error) {
-            console.error("Error uploading file:", error)
-            throw new ValidationError([{ field: 'modFile', message: "An error occurred during file upload." }])
+          const manifest = readManifest(file);
+          
+          if (!semver.valid(manifest.version)) {
+            throw new ValidationError([{ field: 'modFile', message: "Invalid mod version provided." }])
           }
 
-          await prisma.modVersion.create({
-            data: {
-              version,
-              changelog: "First release",
-              downloadUrl: `${Bun.env.FILE_DOWNLOAD_ENDPOINT}/${filename}`,
-              isLatest: true,
-              filename,
-              extension: ext,
-              mod: {
-                connect: {
-                  id: mod.id,
-                }
-              }
-            }
-          })
-
-          return mod
+          return {
+            name: manifest?.name ?? "",
+            version: manifest.version,
+            description: manifest?.description ?? "",
+          }
         }, {
             body: t.Object({
-                name: t.String(),
-                shortDescription: t.String(),
-                description: t.String(),
-                version: t.String(),
-                isNSFW: t.String(),
-                category_id: t.String(),
                 modFile: t.File({ minSize: 1, maxSize: MOD_FILE_SIZE_LIMIT }),
-                modThumbnail: t.File({ type: 'image/png', minSize: 1, maxSize: 8 * 1024 * 1024 }),
             }),
         }
     )
