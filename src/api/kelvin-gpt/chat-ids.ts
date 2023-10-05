66811993d832a179559c738d9cb27528bb298767
Luis Choque <luis.choque.castro@outlook.com>
2023-10-05 14:02:56 -0500

    Fixed chat ids

--- src/api/kelvin-gpt/chat-ids.ts
+++ src/api/kelvin-gpt/chat-ids.ts
@@ -12,11 +12,19 @@ export const router = new Elysia()
             if (user?.canApprove !== true) {
                 throw new NotFoundError();
             }
-            const chatIds = await prisma.kelvinGPTMessages.groupBy({
-                by: ['chatId']
+            const chatIdsWithLatestUpdatedAt = await prisma.kelvinGPTMessages.groupBy({
+                by: ['chatId'],
+                _max: {
+                    updatedAt: true,
+                },
             })
-            return chatIds.map(chat => chat.chatId)
-        }, {
-            response: t.Array(t.String())
+            console.log(chatIdsWithLatestUpdatedAt);
+            const chatIdsOrderedByUpdatedAt = chatIdsWithLatestUpdatedAt.sort((a, b) =>
+                b?._max?.updatedAt?.getTime() && a?._max?.updatedAt?.getTime()
+                ? b._max.updatedAt.getTime() - a._max.updatedAt.getTime()
+                : 0
+            ).map(chat => ({chatId: chat.chatId, updatedAt: chat._max.updatedAt}))
+            
+            return chatIdsOrderedByUpdatedAt
         }
     )
