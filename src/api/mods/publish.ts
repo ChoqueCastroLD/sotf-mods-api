54f22ede7d863ac90915c7743e4778b036ed0f47
Luis Choque <luis.choque.castro@outlook.com>
2024-02-20 19:05:49 -0500

    a new test

--- src/api/mods/publish.ts
+++ src/api/mods/publish.ts
@@ -75,7 +75,7 @@ export const router = new Elysia()
             console.log("6");
             
             const slug = slugify(name, { lower: true });
-            console.log("---7");
+            console.log("-2222222222222--7");
             console.log("name", name);
             console.log("slug", slug);
             console.log("mod_id", mod_id);
