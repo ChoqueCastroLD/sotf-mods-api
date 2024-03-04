398710eef1b5d3f48aa2d0d53183d8a32767f3f8
Luis Choque <luis.choque.castro@outlook.com>
2024-03-04 12:03:33 -0500

    Featured cache

--- src/api/mods/featured.ts
+++ src/api/mods/featured.ts
@@ -5,11 +5,17 @@ import { timeAgo } from '../../shared/time-ago';
 import { authMiddleware } from '../../middlewares/auth.middleware';
 
 
+const featuredModsCache: any = {};
+
 export const router = new Elysia()
     .use(authMiddleware({ loggedOnly: false }))
     .get(
         '/api/mods/featured',
         async ({ user }) => {
+            const query_stringified = JSON.stringify({ user });
+            if (featuredModsCache[query_stringified] && featuredModsCache[query_stringified].expires_at > Date.now()) {
+                return featuredModsCache[query_stringified].data;
+            }
             const mods = await prisma.mod.findMany({
                 where: {
                     isNSFW: false,
@@ -119,6 +125,13 @@ export const router = new Elysia()
                 }
             })
 
-            return allMods.sort((a, b) => b.last_week_downloads - a.last_week_downloads).slice(0, 3);
+            const result = allMods.sort((a, b) => b.last_week_downloads - a.last_week_downloads).slice(0, 3);
+
+            featuredModsCache[query_stringified] = {
+                expires_at: Date.now() + 1000 * 60 * 5, // 5 minutes
+                data: result
+            }
+
+            return result;
         }
     )
