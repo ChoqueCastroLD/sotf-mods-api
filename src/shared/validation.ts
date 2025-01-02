e576f38c16a9988eadc71280aa5bc5c8b7b24a78
Luis Choque <luis.choque.castro@outlook.com>
2025-01-02 18:07:59 -0500

    name size 64

--- src/shared/validation.ts
+++ src/shared/validation.ts
@@ -25,10 +25,10 @@ export function validatePassword(password: string, confirm_password: string) {
 
 export function validateUsername(username: string) {
     const errors = [];
-    if (username.length < 4)
-        errors.push({ field: 'username', message: "Name must be at least 4 characters long" });
-    if (username.length > 24)
-        errors.push({ field: 'username', message: "Name must be at most 24 characters long" });
+    if (username.length < 3)
+        errors.push({ field: 'username', message: "Name must be at least 3 characters long" });
+    if (username.length > 64)
+        errors.push({ field: 'username', message: "Name must be at most 64 characters long" });
     if (username.search(/^[a-z0-9]+$/i) < 0)
         errors.push({ field: 'username', message: "Name must contain only alphanumeric characters" });
     return errors;
@@ -37,11 +37,11 @@ export function validateUsername(username: string) {
 export function validateModName(modName: string) {
     const errors = [];
     let name = modName.trim();
-    if (name.length < 4) {
+    if (name.length < 3) {
       errors.push({ field: 'name', message: "Mod name must be at least 3 characters long."});
     }
-    if (name.length > 24) {
-      errors.push({ field: 'name', message: "Mod name must be less than 50 characters long."});
+    if (name.length > 64) {
+      errors.push({ field: 'name', message: "Mod name must be less than 64 characters long."});
     }
     const allowedPattern = /^[a-zA-Z0-9,.¡!¿?$%&()#+;'" _-]+$/;
     if (!allowedPattern.test(name)) {
@@ -53,8 +53,8 @@ export function validateModName(modName: string) {
   export function validateModDescription(modDescription: string) {
     const errors = [];
     let description = modDescription.trim();
-    if (description.length < 10) {
-      errors.push({ field: 'description', message: "Mod description must be at least 10 characters long."});
+    if (description.length < 3) {
+      errors.push({ field: 'description', message: "Mod description must be at least 3 characters long."});
     }
     if (description.length > 2000) {
       errors.push({ field: 'description', message: "Mod description must be less than 2000 characters long. ("+description.length+")"});
@@ -65,11 +65,11 @@ export function validateModName(modName: string) {
   export function validateModShortDescription(modShortDescription: string) {
     const errors = [];
     let shortDescription = modShortDescription.trim();
-    if (shortDescription.length < 10) {
-      errors.push({ field: 'shortDescription', message: "Mod description must be at least 10 characters long."});
+    if (shortDescription.length < 3) {
+      errors.push({ field: 'shortDescription', message: "Mod description must be at least 3 characters long."});
     }
-    if (shortDescription.length > 100) {
-      errors.push({ field: 'shortDescription', message: "Mod description must be less than 100 characters long. ("+modShortDescription.length+")"});
+    if (shortDescription.length > 200) {
+      errors.push({ field: 'shortDescription', message: "Mod description must be less than 200 characters long. ("+modShortDescription.length+")"});
     }
     return errors;
   }
